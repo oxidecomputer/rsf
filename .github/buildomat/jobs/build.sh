49816710e9dfebf16f1a9c56d1649571bbe7e9c0
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2025-04-29 15:39:12 -0700

    add ci

---
+++ .github/buildomat/jobs/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#:
+#: name = "build"
+#: variety = "basic"
+#: target = "helios-2.0"
+#: rust_toolchain = "stable"
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+banner "check"
+cargo fmt -- --check
+cargo clippy --all-targets -- --deny warnings
+
+banner "test"
+cargo install cargo-nextest
+cargo nextest run
